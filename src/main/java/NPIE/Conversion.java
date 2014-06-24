1a7919998386c8735936182a6e48714ba6f94299
mysticol <rolland.baptiste@gmail.com>
2014-06-24 22:59:12 +0200

    Comments

--- src/main/java/NPIE/Conversion.java
+++ src/main/java/NPIE/Conversion.java
@@ -2,41 +2,71 @@ package NPIE;
 
 public class Conversion {
 
-	// Attributes
-	private Unit source;
-	private Unit target;
-	private double amount;
-	
-	// Constructor
-	public Conversion (Unit source, Unit target, double amount) {
-		this.source = source;
-		this.target = target;
-		this.amount=amount;
-	}
+    private Unit source;
+    private Unit target;
+    private double amount;
 
-	// Getters and Setters :
-	public Unit getSource() {
-		return source;
-	}
+    /**
+     * Conversion Constructor
+     *
+     * @param source the source of the conversion
+     * @param target the target of the conversion
+     * @param amount the amount of the conversion
+     */
+    public Conversion(Unit source, Unit target, double amount) {
+        this.source = source;
+        this.target = target;
+        this.amount = amount;
+    }
 
-	public void setSource(Unit source) {
-		this.source = source;
-	}
+    /**
+     * Return the source of the conversion
+     *
+     * @return the source of the conversion
+     */
+    public Unit getSource() {
+        return source;
+    }
 
-	public Unit getTarget() {
-		return target;
-	}
+    /**
+     * Edit the source of the conversion
+     * @param source the source of the conversion
+     */
+    public void setSource(Unit source) {
+        this.source = source;
+    }
 
-	public void setTarget(Unit target) {
-		this.target = target;
-	}
+    /**
+     * Return the source of the conversion
+     *
+     * @return the source of the conversion
+     */
+    public Unit getTarget() {
+        return target;
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    /**
+     * Edit the target of the conversion
+     * @param target the target of the conversion
+     */
+    public void setTarget(Unit target) {
+        this.target = target;
+    }
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
+    /**
+     * Return the amount of the conversion
+     *
+     * @return the amount of the conversion
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * Edit the amount of the conversion
+     * @param amount of the conversion
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
-
