12fad0cdab294ef41ae0f095d0024627c9165dd2
mysticol <rolland.baptiste@gmail.com>
2014-06-24 23:02:20 +0200

    Comments

--- src/main/java/NPIE/SimpleConversion.java
+++ src/main/java/NPIE/SimpleConversion.java
@@ -2,12 +2,21 @@ package NPIE;
 
 public class SimpleConversion extends Conversion {
 
-	public SimpleConversion(Unit source, Unit target, double amount) {
-		super(source, target, amount);
-	}
-	
-	public double SimpleConv(){
-		return (getSource().getFactor()*getAmount())/getTarget().getFactor();
-	}
-}
+     /**
+     * Conversion Constructor
+     *
+     * @param source the source of the Simpleconversion
+     * @param target the target of the Simpleconversion
+     * @param amount the amount of the Simpleconversion
+     */
+    public SimpleConversion(Unit source, Unit target, double amount) {
+        super(source, target, amount);
+    }
 
+    /**
+     * @return the result of the simple conversion
+     */
+    public double SimpleConv() {
+        return (getSource().getFactor() * getAmount()) / getTarget().getFactor();
+    }
+}
